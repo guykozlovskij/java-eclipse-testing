cbf8d5b90118fcb9ee5f418e8a8a22564276118d
Guy Kozlovskij <gajus.kozlovskij@gmail.com>
2023-08-29 10:56:19 +0100

    more

--- src/Animal.java
+++ src/Animal.java
@@ -3,4 +3,25 @@ public class Animal {
 	public static String iAmDog() {
 		return "I am a dog";
 	}
+	
+	public static void doStuff() {
+		int a = 5;
+		
+		if(a == 0 ) {
+			
+		} else if (a == 1) {
+			
+		} else {
+			
+		}
+		
+		for(int i = 0; i < 5; i++) {
+			
+		}
+		
+		while(a < 50) {
+			System.out.println("hi");
+			a++;
+		}
+	}
 }
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
 
- class HelloWorld {
+class HelloWorld {
 
 	 @SuppressWarnings("unused")
 	public static void main(String[] args) {
 		    System.out.println("Hello");
+		    
+// class is a java file that helps us make objects
 
 //		    int a = 5;
 		    char b = 'i';
@@ -22,6 +25,11 @@
 		    Animal a = new Animal();
 		    String dog = a.iAmDog();
 		    System.out.println(dog);
+		    
+		    
+		    ArrayList<Integer> arr = new ArrayList<Integer>();
+		    
+		    a.doStuff();
 		    
 		  }
 
